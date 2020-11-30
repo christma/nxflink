1521caf604571bb5f61005e9a7684e16dd514870
Christmas <46958618+christma@users.noreply.github.com>
2020-11-30 10:48:19 +0800

    Update ScalaWC.scala

--- src/main/scala/org/example/ScalaWC.scala
+++ src/main/scala/org/example/ScalaWC.scala
@@ -1,8 +1,22 @@
 package org.example
 
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.streaming.api.scala._
+
 object ScalaWC {
   def main(args: Array[String]): Unit = {
-    
+    val hostname = ParameterTool.fromArgs(args).get("hostname")
+    val port = ParameterTool.fromArgs(args).get("port").toInt
+
+
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+    val dataStream = env.socketTextStream(hostname, port)
+    val wc = dataStream.flatMap(_.split(","))
+      .map((_, 1)).keyBy(0).sum(1)
+    wc.print()
+
+    env.execute("ScalaWC")
   }
 
 }
