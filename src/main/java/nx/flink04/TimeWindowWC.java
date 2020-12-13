d4ccc95e826058c819cd96648c5d1ba70d62fc30
christma <liuchuanmumumu@gmail.com>
2020-12-13 09:45:44 +0800

    TimeWindowWC

---
+++ src/main/java/nx/flink04/TimeWindowWC.java
@@ -0,0 +1,36 @@
+package nx.flink04;
+
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+public class TimeWindowWC {
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStreamSource<String> source = env.socketTextStream("127.0.0.1", 9999);
+
+        SingleOutputStreamOperator<Tuple2<String, Integer>> res = source.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
+            @Override
+            public void flatMap(String line, Collector<Tuple2<String, Integer>> collector) throws Exception {
+                String[] fields = line.split(",");
+                for (String word : fields) {
+                    collector.collect(Tuple2.of(word, 1));
+                }
+            }
+        }).keyBy(0)
+                .timeWindow(Time.seconds(10), Time.seconds(5))
+                .sum(1);
+
+        res.print().setParallelism(1);
+
+
+        env.execute("TimeWindowWC");
+    }
+}
