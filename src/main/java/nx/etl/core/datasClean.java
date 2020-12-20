6f5d32fcc5715d0f343c14a38f9d278b0b121833
christma <liuchuanmumumu@gmail.com>
2020-12-20 21:26:32 +0800

    add dependency

--- src/main/java/nx/etl/core/datasClean.java
+++ src/main/java/nx/etl/core/datasClean.java
@@ -4,9 +4,12 @@ import com.sun.xml.internal.bind.v2.TODO;
 import nx.etl.source.custRedisSource;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
@@ -18,7 +21,20 @@ import java.util.Properties;
 
 public class datasClean {
     public static void main(String[] args) throws Exception {
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
+
+
+        env.setParallelism(2);
+        //设置checkpoint的参数
+        env.enableCheckpointing(60000);
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        env.getCheckpointConfig().setMinPauseBetweenCheckpoints(10000);
+        env.getCheckpointConfig().setMaxConcurrentCheckpoints(1);
+        env.getCheckpointConfig().enableExternalizedCheckpoints(
+                CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+
+
+
 
         Properties prop = new Properties();
         prop.put("bootstrap.servers", "localhost:9092");
@@ -54,7 +70,9 @@ public class datasClean {
 
 //        streamOperator.print();
 
+
         streamOperator.addSink(producer);
+
 
 
         env.execute("datasClean");
