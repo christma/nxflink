a9b27a8785641349d772ad61fe426467419328d1
christma <liuchuanmumumu@gmail.com>
2020-12-26 12:37:46 +0800

    add watermark

--- src/main/java/nx/adClickCount/AdClockCount.java
+++ src/main/java/nx/adClickCount/AdClockCount.java
@@ -2,11 +2,18 @@ package nx.adClickCount;
 
 import nx.utils.Utils;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
 import org.apache.flink.streaming.api.watermark.Watermark;
+import org.apache.flink.util.Collector;
 
 import javax.annotation.Nullable;
 
@@ -19,6 +26,10 @@ public class AdClockCount {
         SingleOutputStreamOperator<AdClickEvent> adEventStream = env.readTextFile(Utils.CLICK_LOG_PATH)
                 .map(new parseAdClickLog())
                 .assignTimestampsAndWatermarks(new AdClickEventTimeExtractor());
+
+
+        adEventStream.keyBy(new getKey()).process(new FilterBlackListUser(100))
+
 
         env.execute("AdClockCount");
     }
@@ -122,6 +133,48 @@ public class AdClockCount {
             currentMaxEventTime = Math.max(currentMaxEventTime, adClickEvent.timestamp);
 
             return timestamp;
+        }
+    }
+
+    private static class getKey implements KeySelector<AdClickEvent, Tuple2<Long, Long>> {
+        @Override
+        public Tuple2<Long, Long> getKey(AdClickEvent adClickEvent) throws Exception {
+            return Tuple2.of(adClickEvent.getUserId(), adClickEvent.adId);
+        }
+    }
+
+    private static class FilterBlackListUser extends KeyedProcessFunction<Tuple2<Long, Long>, AdClickEvent, AdClickEvent> {
+        public int maxCount;
+        public ValueState<Long> countState;
+        public ValueState<Boolean> isSetBlackList;
+        public ValueState<Long> resetTimeer;
+
+
+        @Override
+        public void open(Configuration parameters) throws Exception {
+
+            ValueStateDescriptor<Long> descriptor = new ValueStateDescriptor<>("count-state", Long.class);
+            countState = getRuntimeContext().getState(descriptor);
+
+            ValueStateDescriptor<Boolean> descriptor1 = new ValueStateDescriptor<>("issent-state", Boolean.class);
+            new ValueStateDescriptor<>("issent-state", Boolean.class);
+
+            isSetBlackList = getRuntimeContext().getState(descriptor1);
+
+            ValueStateDescriptor<Long> descriptor2 = new ValueStateDescriptor<>("resettime-state", Long.class);
+
+            resetTimeer = getRuntimeContext().getState(descriptor2);
+
+
+        }
+
+        public FilterBlackListUser(int maxCount) {
+            this.maxCount = maxCount;
+        }
+
+        @Override
+        public void processElement(AdClickEvent adClickEvent, Context context, Collector<AdClickEvent> collector) throws Exception {
+
         }
     }
 }
