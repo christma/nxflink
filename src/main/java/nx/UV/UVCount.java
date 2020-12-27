1ade0ef4361319225c1687915c513b4ef912dae0
christma <liuchuanmumumu@gmail.com>
2020-12-27 12:51:02 +0800

    UVCount

--- src/main/java/nx/UV/UVCount.java
+++ src/main/java/nx/UV/UVCount.java
@@ -18,7 +18,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-public class UVCounet {
+public class UVCount {
     public static void main(String[] args) throws Exception {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
